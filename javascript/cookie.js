ad5d6b9960a2afb58fc5951cfa8cdab2356bcd09
hajar424 <promixro@gmail.com>
2015-08-07 02:32:56 +0200

    dollar insted off cookeis and local file

--- javascript/cookie.js
+++ javascript/cookie.js
@@ -1,29 +1,34 @@
 var cookies = 0;
+var stage1 =  9;
+var stage2 =  19;
+var stage3 =  29;
 
-var stage1 =  10;
-var stage2 =  20;
-var stage3 =  30;
-
-function clickt(){
-  document.getElementById("cookie_img").style.height = "250px";
-
-  cookies++;
-  document.getElementById("counter").innerHTML = "Cookies: "+cookies;
-  //Rainbow cookies
-  switch (cookies) {
-    case stage1:
-
-        document.getElementById("ac_1").innerHTML = "- Unlockt cookie monster "+stage1;
-        break;
-    case stage2:
-          document.getElementById("ac_2").innerHTML = "- Found a big Hunny pot "+ stage2;
+function xd (){
+        document.getElementById("result").innerHTML = "Money: $" + localStorage.clickcount ;
           document.body.style.backgroundColor = "orange";
-          break;
-    case stage3:
-              document.getElementById("ac_3").innerHTML = "- You won! " + stage3;
-            document.body.style.backgroundColor = "#18C518";
-            break;
+}
+function clickt(){
+
+
+
+    cookies++;
+  if(typeof(Storage) !== "undefined") {
+      if (localStorage.clickcount) {
+          localStorage.clickcount = Number(localStorage.clickcount)+1;
+      } else {
+          localStorage.clickcount = 1;
+      }
+      document.getElementById("result").innerHTML = "Money: $" + localStorage.clickcount ;
+  } else {
+      document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
   }
+
+
+  document.getElementById("counter").innerHTML = "Cookies: $"+cookies;
+
+
+  //Rainbow cookies
+
 
 
 }
